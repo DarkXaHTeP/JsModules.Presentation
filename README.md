55c61dab530e60f975420d218481751c57723556
Volodymyr Osmak <darkxahtep@gmail.com>
2016-04-13 02:40:50 +0300

    Initial version

---
+++ README.md
@@ -0,0 +1,6 @@
+# Presentation about JavaScript modules using Reveal.JS
+
+It is built and deployed to GitHub Pages with the help of Travis CI.
+You can find live version of the presentation [here](https://darkxahtep.github.io/jsmodules.presentation).
+
+#### License: [MIT](LICENSE)
---
+++ public/scripts/main.js
@@ -0,0 +1,49 @@
+Reveal.initialize({
+
+    // Display controls in the bottom right corner
+    controls: true,
+
+    // Display a presentation progress bar
+    progress: true,
+
+    // Display the page number of the current slide
+    slideNumber: false,
+
+    // Push each slide change to the browser history
+    history: true,
+
+    // Enable keyboard shortcuts for navigation
+    keyboard: true,
+
+    // Enable the slide overview mode
+    overview: false,
+
+    // Vertical centering of slides
+    center: true,
+
+    // Enables touch navigation on devices with touch input
+    touch: true,
+
+    // Loop the presentation
+    loop: false,
+
+    // Flags if the presentation is running in an embedded mode,
+    // i.e. contained within a limited portion of the screen
+    embedded: false,
+
+    // Flags if we should show a help overlay when the questionmark
+    // key is pressed
+    help: false,
+
+    // Flags if speaker notes should be visible to all viewers
+    showNotes: false,
+
+    // Transition style
+    transition: 'convex', // none/fade/slide/convex/concave/zoom
+
+    // Transition speed
+    transitionSpeed: 'slow', // default/fast/slow
+
+    // Transition style for full page slide backgrounds
+    backgroundTransition: 'fade' // none/fade/slide/convex/concave/zoom
+});
